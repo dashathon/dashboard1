22e645a1d13dc19ce4b4158c9096c30c9bedc548
Simd12 <simon.degenhardt@stud.uni-due.de>
2022-06-11 12:21:46 +0200

    -Add remaining sections

--- dashboard-client/src/models/interfaces/IndividualProgrammingExperience.ts
+++ dashboard-client/src/models/interfaces/IndividualProgrammingExperience.ts
@@ -2,7 +2,7 @@ import { fivePointLikertScale } from "../enums/FivePointLikertScale";
 
 export interface IndividualProgrammingExperience {
     ProgrammingExperienceYears: number[];
-    ProgrammingExperienceComparison: fivePointLikertScale[][];
-    ProgrammingAbility: {[key: string]:fivePointLikertScale[[]]};
+    ProgrammingExperienceComparison: fivePointLikertScale[];
+    ProgrammingAbility: {[key: string]:fivePointLikertScale[]};
     ProgrammingComfort: {[key: string]:fivePointLikertScale[]};
 }
--- dashboard-client/src/services/parseService.ts
+++ dashboard-client/src/services/parseService.ts
@@ -4,6 +4,10 @@ import { CommunityMeasures } from "src/models/interfaces/CommunityMeasures";
 import { IndividualMeasures } from "src/models/interfaces/IndividualMeasures";
 import { TeamComposition } from "src/models/interfaces/TeamComposition";
 import { TeamProcess } from "src/models/interfaces/TeamProcess";
+import { Mentoring } from "src/models/interfaces/Mentoring";
+import { IndividualProgrammingExperience } from "src/models/interfaces/IndividualProgrammingExperience";
+import { role } from "src/models/enums/Role";
+
 
 export class parseService {
 //#region Instances
@@ -20,11 +24,29 @@ export class parseService {
         TeamLeader: [],
         TeamManager: [],
         SocialLeader: [],
-    }
+    };
+    public mentoring : Mentoring = {
+        MentoringExperience: {},
+        MentoringOther: [],
+    };
+
+    public communityMeasures : CommunityMeasures = {
+        CommunityIdentification: {},
+        SocialCapital: {},
+    };
+
+    public individualProgrammingExperience : IndividualProgrammingExperience = {
+        ProgrammingExperienceYears: [],
+        ProgrammingExperienceComparison: [],
+        ProgrammingAbility: {},
+        ProgrammingComfort: {},
+    };
+
+    
 //#endregion
 //#region exampleData
-    readonly as : string[] = [
-        'Team1', 'asfmasf'
+    readonly teams : string[] = [
+        'Team1', 'Team2', 'Team3', 'Team4', 'Team5'
     ];
     readonly exampleString : string = 'exampleString';
 //#endregion
@@ -68,6 +90,31 @@ export class parseService {
         }
     }
 
+    getRole(answer: number): number {
+        switch(answer) { 
+            case 0: { 
+               return role.Yes; 
+               break; 
+            } 
+            case 1: { 
+               return role.SomeOneElse; 
+               break; 
+            }
+            case 2: { 
+                return role.Nobody; 
+                break; 
+             }
+            
+            default: { 
+                throw new Error('number out of range')
+               //statements; 
+               break; 
+            } 
+        }
+    }
+
+
+
 
      insertIndividualMeasures() : void{
         this.individualMeasures.Motivation['Making something cool / Working on an interesting project idea'].push(this.getFivePointScala(this.getRandomInt(5)));
@@ -85,28 +132,77 @@ export class parseService {
         this.individualMeasures.EventParticipation.push(this.getRandomInt(5));
     }
 
-    /*insertTeamProcess() : void{
-        const tm : TeamProcess = {
-            SatisfactionWithProcess:{
-                Effiency: this.getFivePointScala(this.getRandomInt(5)),
-                Coordination: this.getFivePointScala(this.getRandomInt(5)),
-                Fairness: this.getFivePointScala(this.getRandomInt(5)),
-                Understanding: this.getFivePointScala(this.getRandomInt(5)),
-            },
-            GoalClarty:{
-                'I was uncertain of my duties and responsibilities in this team.' : this.getFivePointScala(this.getRandomInt(5)),
-                'I was unclear about the goals and objectives for my work in this team.' : this.getFivePointScala(this.getRandomInt(5)),
-                'I was unsure how my work relates to the overall objectives of my team.' : this.getFivePointScala(this.getRandomInt(5)),
-            },
-            Voice:{
-                'Everyone had a chance to express his/her opinion.' : this.getFivePointScala(this.getRandomInt(5)),
-                'The team members responded to the comments made by others.' : this.getFivePointScala(this.getRandomInt(5)),
-                'The team members participated very actively during the project.' : this.getFivePointScala(this.getRandomInt(5)),
-                'Overall, the participation of each member in the team was effective.' : this.getFivePointScala(this.getRandomInt(5)),
 
-            }
-        }
+    insertTeamComposition() : void{
+            this.teamComposition.TeamId.push(this.teams[this.getRandomInt(5)]);
+            this.teamComposition.TeamSize.push(this.getRandomInt(5));
+            this.teamComposition.CodeRepo.push(this.exampleString);
+            this.teamComposition.ResourcesURL.push(this.exampleString);
+            this.teamComposition.TeamLeader.push(this.getRole(this.getRandomInt(3)));
+            this.teamComposition.TeamManager.push(this.getRole(this.getRandomInt(3)));
+            this.teamComposition.TeamLeader.push(this.getRole(this.getRandomInt(3)));
     }
-    */
+
+
+    insertMentoring() : void{
+            this.mentoring.MentoringExperience['The mentors supported us to scope our project.'].push(this.getFivePointScala(this.getRandomInt(5)));
+            this.mentoring.MentoringExperience['The mentors took decisions about the direction of our project.'].push(this.getFivePointScala(this.getRandomInt(5)));
+            this.mentoring.MentoringExperience['The mentors provided us with solutions to technical problems we encountered.'].push(this.getFivePointScala(this.getRandomInt(5)));
+            this.mentoring.MentoringExperience['The mentors helped us to find our own solutions to technical problems we encountered.'].push(this.getFivePointScala(this.getRandomInt(5)));
+            this.mentoring.MentoringExperience['The mentor were mainly focused on our learning progress.'].push(this.getFivePointScala(this.getRandomInt(5)));
+            this.mentoring.MentoringExperience['The mentors were mainly focused on our project progress.'].push(this.getFivePointScala(this.getRandomInt(5)));
+            this.mentoring.MentoringExperience['The mentors were part of our team.'].push(this.getFivePointScala(this.getRandomInt(5)));
+            this.mentoring.MentoringExperience['The mentors showed interest in us beyond the project we were working on.'].push(this.getFivePointScala(this.getRandomInt(5)));
+            this.mentoring.MentoringExperience['We could reach the mentors when we needed help.'].push(this.getFivePointScala(this.getRandomInt(5)));
+
+            this.mentoring.MentoringOther.push(this.exampleString);
+    }
+
+    insertCommunityMeasures() : void{
+        this.communityMeasures.CommunityIdentification['I identify with other members of this community.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.communityMeasures.CommunityIdentification['I am like other members of this community.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.communityMeasures.CommunityIdentification['This community is an important reflection of who I am.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.communityMeasures.CommunityIdentification['I would like to continue working with this community.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.communityMeasures.CommunityIdentification['I dislike being a member of this community.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.communityMeasures.CommunityIdentification['I would rather belong to another community.'].push(this.getFivePointScala(this.getRandomInt(5)));
+
+        this.communityMeasures.SocialCapital['I feel I am part of this community.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.communityMeasures.SocialCapital['I am interested in what goes on in this community.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.communityMeasures.SocialCapital['Interacting with other members of this community makes me want to try new things.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.communityMeasures.SocialCapital['I am willing to spend time to support general activities in this community.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.communityMeasures.SocialCapital['Through this community I come into contact with new people all the time.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.communityMeasures.SocialCapital['There are several people in this community that I trust to help me solve my problems.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.communityMeasures.SocialCapital['I know someone in this community that I can turn to if I urgently need help.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.communityMeasures.SocialCapital['There is someone in this community that I can turn to for advice about making important decision.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.communityMeasures.SocialCapital['The other members of this community would be good job references for me.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.communityMeasures.SocialCapital['I do not know people in this community well enough to get them to do anything important.'].push(this.getFivePointScala(this.getRandomInt(5)));
+    }
+
+    insertIndividualProgrammingExperience() : void {
+        this.individualProgrammingExperience.ProgrammingExperienceYears.push(this.getRandomInt(5));
+        this.individualProgrammingExperience.ProgrammingExperienceComparison.push(this.getFivePointScala(this.getRandomInt(5)));
+        
+        this.individualProgrammingExperience.ProgrammingAbility['I am able to write some parts of programs in JAVA.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.individualProgrammingExperience.ProgrammingAbility['I am able to write some parts of programs in JAVASCRIPT.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.individualProgrammingExperience.ProgrammingAbility['I am able to write some parts of programs in PYTHON.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.individualProgrammingExperience.ProgrammingAbility['I am able to write some parts of programs in HTML/HTML5.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.individualProgrammingExperience.ProgrammingAbility['I am able to write some parts of programs in DJANGO/AIRAVATA.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.individualProgrammingExperience.ProgrammingAbility['I can make some use of REQUESTS, JSON and XML.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.individualProgrammingExperience.ProgrammingAbility['I can make some use of JUPYTER NOTEBOOKS.'].push(this.getFivePointScala(this.getRandomInt(5)));
+
+        this.individualProgrammingExperience.ProgrammingComfort['I am comfortable to write programs in JAVA.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.individualProgrammingExperience.ProgrammingComfort['I am comfortable to write programs in JAVASCRIPT.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.individualProgrammingExperience.ProgrammingComfort['I am comfortable to write programs in PYTHON.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.individualProgrammingExperience.ProgrammingComfort['I am comfortable to write programs in HTML/HTML5.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.individualProgrammingExperience.ProgrammingComfort['I am comfortable to write programs in DJANGO/AIRAVATA.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.individualProgrammingExperience.ProgrammingComfort['I am comfortable to use REQUESTS, JSON and XML.'].push(this.getFivePointScala(this.getRandomInt(5)));
+        this.individualProgrammingExperience.ProgrammingComfort['I am comfortable to use JUPYTER NOTEBOOKS.'].push(this.getFivePointScala(this.getRandomInt(5)));
+
+
+    }
+
+
+
+
 
   }
