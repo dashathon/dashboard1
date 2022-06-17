a7baf1866333d82d4e536e510194bc194bcec625
HelgeGebel <helge.gebel@gmail.com>
2022-06-17 13:23:48 +0200

    Implemented Pipes
    mentoring-, individual-measures-, demographics-, programming- & project-measure-pipes implemented, as well as some minor changes towards optional items in the project measures

--- dashboard-client/src/app/open-answer-list/open-answer-list.component.ts
+++ dashboard-client/src/app/open-answer-list/open-answer-list.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class OpenAnswerListComponent implements OnInit {
 
   @Input()
-  answerList = Array<string>();
+  answerList = Array<any>();
 
   @Input()
   captionText?: string;
