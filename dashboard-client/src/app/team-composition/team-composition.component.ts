d3beb954c54212759a851181605dbc2e449c4792
AIRU26 <105240520+AIRU26@users.noreply.github.com>
2022-06-21 21:46:33 +0200

    add @Input formData

---
+++ dashboard-client/src/app/team-composition/team-composition.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { TeamComposition } from 'src/models/interfaces/TeamComposition';
+@Component({
+  selector: 'app-team-composition',
+  templateUrl: './team-composition.component.html',
+  styleUrls: ['./team-composition.component.scss']
+})
+export class TeamCompositionComponent implements OnInit {
+  @Input()
+  formData?: any;
+  
+
+ 
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
